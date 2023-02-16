dcfd82f76a3d85bb86d5f4c893a75e0d8d8336a7
Turku Gamelab <78027203+ThomVon@users.noreply.github.com>
2023-02-16 12:46:49 +0200

    VALMIS

--- Ar testing/easyar test/Assets/thomas Scripts/FreezeScript.cs
+++ Ar testing/easyar test/Assets/thomas Scripts/FreezeScript.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
+
 public class FreezeScript : MonoBehaviour
 {
-    public GameObject stereo;
 
-    void Start()
+    public GameObject tracking;
+    public GameObject frezbut;
+    
+
+    
+
+   
+
+    public void FreezePosition()
     {
-        stereo = GetComponent<m_Rigidbody>();
+        if (tracking.activeSelf == false)
+        {
+            tracking.SetActive(true);
+            frezbut.SetActive(false);
+        }
+        else
+        {
+            tracking.SetActive(false);
+            frezbut.SetActive(true);
+        }
+
+        
     }
 
-    void FreezePosition()
-    {
-        m_Rigidbody.constraints = RigidbodyConstraints.FreezePosition;
-    }
+    
 }
